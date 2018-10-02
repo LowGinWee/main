03a27cf7a0c372e211a2bc51a4262b6293d91222
LowGinWee <notginwee@gmail.com>
2018-10-02 19:59:23 +0800

    Added UniqueTagList (not complete)

---
+++ src/main/java/seedu/address/model/tag/exceptions/TagNotFoundException.java
@@ -0,0 +1,9 @@
+//@@author LowGinWee
+package seedu.address.model.tag.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified person.
+ */
+public class TagNotFoundException extends RuntimeException{
+
+}
